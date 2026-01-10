e5647eb0b29e3bf25c399e0065a55065af453a14
digitronics2025 <digitronics.electro@gmail.com>
2026-01-10 13:06:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -241,22 +241,22 @@ const OFFERS_FALLBACK_MESSAGE = `🚨🔥 *PROMO FLASH اليوم* 🔥🚨
 ⚠️ (Stock limité – حتى يكمّل الستوك)
 🚚 *توصيل مجاني* + 🎁 *هدية مع كل TV*
 
-1️⃣ *Samsung 32" HD* (32H5000F)
+1️⃣ *Samsung Smart TV HD 32 HD* (32H5000F)
 💥 *1499 DH فقط!* ✅
 
-2️⃣ *Echolink 32" QLED* (Q32WM)
+2️⃣ *Echolink Smart Tv 32 Android Qled*
 💥 *1199 DH فقط!* ✅
 
-3️⃣ *Morsat 32" HD*
+3️⃣ *Morsat Tv 32 HD Smart Android*
 💥 *1120 DH فقط!* ✅
 
-4️⃣ *Morsat 43"*
+4️⃣ *Morsat Tv Led 43 FHD Smart Android"*
 💥 *1999 DH فقط!* ✅
 
-5️⃣ *TCL 32" QLED* (32S5K)
+5️⃣ *TCL GoogleTV QLED 32″ Full HD 32S5K*
 💥 *1499 DH فقط!* ✅
 
-6️⃣ *TCL 43" QLED 4K* (P7K)
+6️⃣ *TCL Smart Tv 43 Qled 4k Uhd Google Tv 43P7k*
 💥 *3389 DH* 🎁 *(+ عام اشتراك)* ✅
 
 🛒 *Commande / طلب:* digitronics.ma`;
